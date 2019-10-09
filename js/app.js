e05f2ccaa18830b9dfc8f432a7fc1522389a8f28
TessSayward <Tesskatherinesay@gmail.com>
2019-10-09 13:04:48 -0400

    Earth and Moon

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
 $(document).foundation()
 AOS.init();
+
+$('.button').click(function(event){
+  
+  // prevent defalt click behavior
+  // don't jump to content
+  event.preventDefault();
+    
+  // identify position of target
+  var target = $(this).attr('href');
+  var top = $(target).offset().top;
+
+  // animate scroll to target
+  $('html,body').animate({
+    scrollTop: top
+    },700);
+
+});
