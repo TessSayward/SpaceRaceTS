763fcc186d6745dd9f12c3e9cd4bf2e00c98299b
TessSayward <Tesskatherinesay@gmail.com>
2019-10-13 20:24:56 -0400

    DOne?

--- js/app.js
+++ js/app.js
@@ -17,3 +17,41 @@ $('.button').click(function(event){
     },700);
 
 });
+
+// var sputnik = document.querySelector(".sputnik");
+// var status = 0;
+
+// function setTranslate(xPos, yPos, el) {
+//   el.style.transform = "translate3d(" + xPos + ", " + yPos + "px, 0)";
+// }
+ 
+// window.addEventListener("DOMContentLoaded", scrollLoop, false);
+
+// var xScrollPosition;
+// var yScrollPosition;
+ 
+// function scrollLoop() {
+//   xScrollPosition = window.scrollX;
+//   yScrollPosition = window.scrollY;
+
+//   setTranslate(0, yScrollPosition * -0.2, sputnik);
+  
+//   requestAnimationFrame(scrollLoop);
+// }
+
+// $('.sputnik').click(function(event){
+  
+//   // prevent defalt click behavior
+//   // don't jump to content
+//   event.preventDefault();
+    
+//   // identify position of target
+//   var target = $(this).attr('href');
+//   var top = $(target).offset().top;
+
+//   // animate scroll to target
+//   $('html,body').animate({
+//     scrollTop: top
+//     },700);
+
+// });
